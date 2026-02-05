31ab24ae3903f5421015f4bb2272ed39bbbab6dd
Punay kukreja <kukrejapunay04@gmail.com>
2026-02-06 02:52:18 +0530

    add trpc

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,20 @@
-export default function Home() {
-  return <div className="text-red-500">hola</div>;
+import { getQueryClient, trpc } from "@/trpc/server";
+import ClientComponent from "./client";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+import { Suspense } from "react";
+
+export default async function Home() {
+  const queryClient = getQueryClient();
+
+  void queryClient.prefetchQuery(trpc.getUsers.queryOptions());
+
+  return (
+    <div className="text-red-500">
+      <HydrationBoundary state={dehydrate(queryClient)}>
+        <Suspense fallback={<div>Loading...</div>}>
+          <ClientComponent />
+        </Suspense>
+      </HydrationBoundary>
+    </div>
+  );
 }
